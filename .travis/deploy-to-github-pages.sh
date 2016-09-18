d271b8b91af44724c875b96fecc206e9009ec8b2
Xiao Hanyu <xiaohanyu1988@gmail.com>
2016-09-18 11:30:41 +0800

    Refactoring travis script.

--- .travis/deploy-to-github-pages.sh
+++ .travis/deploy-to-github-pages.sh
@@ -3,6 +3,12 @@
 set -o errexit
 rm -rf output
 
+# install the latest pandoc
+sudo apt-get update && sudo apt-get -y install jq
+# Get the latest .deb released.
+wget `curl https://api.github.com/repos/jgm/pandoc/releases/latest | jq -r '.assets[] | .browser_download_url | select(endswith("deb"))'` -O pandoc.deb
+sudo dpkg -i pandoc.deb
+
 # config
 git config --global user.email "xiaohanyu1988@gmail.com"
 git config --global user.name "Xiao Hanyu"
@@ -10,9 +16,7 @@ git config --global user.name "Xiao Hanyu"
 # build
 bundle install
 
-nanoc_plate_rev=$(git rev-parse HEAD)
-
-git clone https://github.com/${GITHUB_PAGES_REPO}.git output
+git clone "https://${GITHUB_TOKEN}@github.com/${GITHUB_PAGES_REPO}.git" output
 
 bundle exec nanoc compile
 
@@ -28,7 +32,7 @@ else
 Auto deployed by travis-ci.
 
 Projects used for this deployment:
-- https://github.com/nanoc-plate/nanoc-plate/commit/${nanoc_plate_rev}
+- https://github.com/${TRAVIS_REPO_SLUG}/commit/${TRAVIS_COMMIT}
 EOF
 
     git push -f "https://${GITHUB_TOKEN}@github.com/${GITHUB_PAGES_REPO}.git" master:master
