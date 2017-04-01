e7c54f33a294f76d13ad2a99624febd924795f87
Xiao Hanyu <xiaohanyu1988@gmail.com>
2017-04-01 18:54:28 +0800

    Add analytics from https://growing.io

---
+++ content/static/javascripts/growingio.js
@@ -0,0 +1,13 @@
+var _vds = _vds || [];
+window._vds = _vds;
+(function(){
+  _vds.push(['setAccountId', 'ac68c2941772100d']);
+  (function() {
+    var vds = document.createElement('script');
+    vds.type='text/javascript';
+    vds.async = true;
+    vds.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'dn-growing.qbox.me/vds.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(vds, s);
+  })();
+})();
